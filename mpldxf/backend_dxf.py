f24fd219dd288b4149786eca18e5c423739f9151
Ingeborg Gjerde <ingeborg.gjerde@ngi.no>
2026-04-07 10:37:18 +0200

    remove license header in files, we only need the central license file

--- mpldxf/backend_dxf.py
+++ mpldxf/backend_dxf.py
@@ -13,25 +13,6 @@ or register:
   matplotlib.backend_bases.register_backend('dxf', FigureCanvasDxf)
 
 Based on matplotlib.backends.backend_template.py.
-
-Copyright (C) 2014 David M Kent
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 from io import StringIO
