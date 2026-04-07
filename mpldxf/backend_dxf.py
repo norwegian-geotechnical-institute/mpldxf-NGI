0c0e8f62dd94b89a20c6e4788255de7332ca58c3
Ingeborg Gjerde <ingeborg.gjerde@ngi.no>
2026-04-07 11:13:17 +0200

    move text drawing logic into separate file

--- mpldxf/backend_dxf.py
+++ mpldxf/backend_dxf.py
@@ -19,7 +19,6 @@ from io import StringIO
 import os
 import sys
 import math
-import re
 
 import matplotlib
 from matplotlib.backend_bases import (
@@ -33,7 +32,6 @@ import numpy as np
 from shapely import Point
 from shapely.geometry import LineString, Polygon
 import ezdxf
-from ezdxf.enums import TextEntityAlignment
 from ezdxf.math.clipping import ClippingRect2d
 
 from .color_utils import rgb_to_dxf
@@ -43,6 +41,7 @@ from .fm_layers import (
     determine_text_layer,
 )
 from .geometry_utils import filter_invalid_coordinates, is_valid_coordinate
+from .text_drawing import draw_text_entity
 
 class RendererDxf(RendererBase):
     """
@@ -648,98 +647,18 @@ class RendererDxf(RendererBase):
                             )
 
     def draw_text(self, gc, x, y, s, prop, angle, ismath=False, mtext=None):
-        """Draw text with proper layer assignment"""
-        if mtext is None:
-            return
-
-        fontsize = self.points_to_pixels(prop.get_size_in_points()) / 2
-
-        dxfattribs = {}
-        if self.use_fm_layers:
-            layer_name = self._determine_text_layer(s, fontsize)
-            dxfattribs["layer"] = layer_name
-            dxfattribs["color"] = 256
-        else:
-            dxfattribs["color"] = rgb_to_dxf(gc.get_rgb())
-
-        s = s.replace("\u2212", "-")
-        s = s.encode("ascii", "ignore").decode()
-
-        if s and len(s) > 0 and s[0] == "$":
-            pattern = r"\\mathbf\{(.*?)\}"
-            stripped_text = re.sub(pattern, r"\1", s)
-            stripped_text = re.sub(r"[$]", "", stripped_text)
-            stripped_text = re.sub(r"\\/", " ", stripped_text)
-            text = self.modelspace.add_text(
-                stripped_text,
-                height=fontsize,
-                rotation=angle,
-                dxfattribs=dxfattribs,
-            )
-        else:
-            text = self.modelspace.add_text(
-                s,
-                height=fontsize,
-                rotation=angle,
-                dxfattribs=dxfattribs,
-            )
-
-        # Text alignment
-        if angle == 90.0:
-            if mtext._rotation_mode == "anchor":
-                halign = self._map_align(mtext.get_ha(), vert=False)
-            else:
-                halign = "RIGHT"
-            valign = self._map_align(mtext.get_va(), vert=True)
-        else:
-            halign = self._map_align(mtext.get_ha(), vert=False)
-            valign = self._map_align(mtext.get_va(), vert=True)
-
-        # Build alignment string properly (avoid empty valign causing "_LEFT" etc.)
-        if valign and valign != "":
-            align = valign + "_" + halign
-        else:
-            align = halign
-
-        # Ensure align is never empty (AutoCAD cannot handle empty/None alignment)
-        if not align or align == "" or align == "_":
-            align = "LEFT"
-
-        alignment_map = {
-            "TOP_LEFT": TextEntityAlignment.TOP_LEFT,
-            "TOP_CENTER": TextEntityAlignment.TOP_CENTER,
-            "TOP_RIGHT": TextEntityAlignment.TOP_RIGHT,
-            "MIDDLE_LEFT": TextEntityAlignment.MIDDLE_LEFT,
-            "MIDDLE_CENTER": TextEntityAlignment.MIDDLE_CENTER,
-            "MIDDLE_RIGHT": TextEntityAlignment.MIDDLE_RIGHT,
-            "BOTTOM_LEFT": TextEntityAlignment.BOTTOM_LEFT,
-            "BOTTOM_CENTER": TextEntityAlignment.BOTTOM_CENTER,
-            "BOTTOM_RIGHT": TextEntityAlignment.BOTTOM_RIGHT,
-            "LEFT": TextEntityAlignment.LEFT,
-            "CENTER": TextEntityAlignment.CENTER,
-            "RIGHT": TextEntityAlignment.RIGHT,
-        }
-
-        align = alignment_map.get(align, TextEntityAlignment.BOTTOM_LEFT)
-
-        pos = mtext.get_unitless_position()
-        x, y = mtext.get_transform().transform(pos)
-        p1 = x, y
-        text.set_placement(p1, align=align)
-
-    def _map_align(self, align, vert=False):
-        """Translate a matplotlib text alignment to the ezdxf alignment."""
-        if align in ["right", "center", "left", "top", "bottom", "middle"]:
-            align = align.upper()
-        elif align == "baseline":
-            align = ""
-        elif align == "center_baseline":
-            align = "MIDDLE"
-        else:
-            raise NotImplementedError
-        if vert and align == "CENTER":
-            align = "MIDDLE"
-        return align
+        """Draw text with proper layer assignment."""
+        draw_text_entity(
+            self.modelspace,
+            gc,
+            s,
+            prop,
+            angle,
+            mtext,
+            self.points_to_pixels,
+            self.use_fm_layers,
+            self._determine_text_layer,
+        )
 
     def flipy(self):
         return False
